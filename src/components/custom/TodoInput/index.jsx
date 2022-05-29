fa27aaf49aedeb06e42529772ae66668c00ea409
mamad <dehghanseyedmohammad@gmail.com>
2022-05-29 20:45:44 +0330

    extaract todoInput component from todoManager component & solve issue when delete a todoItem thay ready for editing

--- src/components/custom/TodoInput/index.jsx
+++ src/components/custom/TodoInput/index.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import './TodoInput.style.scss'
 
-const TodoInput = ({ inputValue, setInputValue , editTodo, editTodoId, addTodo}) => {
+const TodoInput = ({ inputValue, setInputValue, editTodo, editTodoId, addTodo }) => {
     return (
         <div className='input-section'>
-            
+            <input type="text" value={inputValue} onChange={e => setInputValue(e.target.value)} onKeyDown={e => (e.key === "Enter") && (editTodoId === null ? addTodo() : editTodo())} placeholder='Add a todo' />
+
+            {editTodoId === null ? <button onClick={addTodo}>Add</button> : <button onClick={editTodo}>edit</button>}
         </div>
     );
 };
--- src/components/custom/TodoManager/index.jsx
+++ src/components/custom/TodoManager/index.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { v4 } from 'uuid';
-// import TodoInput from '../TodoInput';
+import TodoInput from '../TodoInput';
 import TodoItem from './../TodoItem';
 import './TodoManager.style.scss';
 
@@ -23,7 +23,10 @@ const TodoManager = () => {
 
 	const removeTodo = (id) => {
 		setTodoList(todoList.filter(item => item.id !== id))
-
+		if(id === editTodoId){
+			setEditTodoId(null);
+			setInputValue('')
+		}
 	}
 
 	const editTodo = () => {
@@ -52,13 +55,8 @@ const TodoManager = () => {
 	return (
 		<div className="todo-manager">
 			<h2>Feelin' productive today?</h2>
-			<div className='input-section'>
-				{/* <TodoInput inputValue={inputValue} setInputValue={setInputValue} addTodo={addTodo} editTodo={editTodo} /> */}
+			<TodoInput inputValue={inputValue} setInputValue={setInputValue} addTodo={addTodo} editTodo={editTodo} editTodoId={editTodoId} />
 
-				<input type="text" value={inputValue} onChange={e => setInputValue(e.target.value)} onKeyDown={e => (e.key === "Enter") && (editTodoId === null ? addTodo(): editTodo())} placeholder='Add a todo' />
-
-				{editTodoId === null ? <button onClick={addTodo}>Add</button> : <button onClick={editTodo}>edit</button>}
-			</div>
 			<div className='todos-section'>
 				{todoList.map(item => (
 					<TodoItem key={item.id} todo={item} remove={() => removeTodo(item.id)} edit={() => loadEditTodoInput(item.id)}></TodoItem>
